a5754402692323e4bb60a8ed3741eb4111c6dc82
salahalddin <salahalddin.tt@gmail.com>
2025-09-26 01:58:27 +0300

    creat the file flutter

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+void main(){
+  runApp(salah());
+}
+class salah extends StatelessWidget {
+  const salah({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
