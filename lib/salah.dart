11c4ce815e7dd919ef306251ca631f5c26289a99
salahalddin <salahalddin.tt@gmail.com>
2025-09-26 14:50:05 +0300

    Navigator.push in the branch salah MaterialPageRoute screan_salah

---
+++ lib/salah.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class screan_salah extends StatelessWidget {
+  const screan_salah({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+      ),
+    );
+  }
+}
